d3e35a9fdcbc915afbe90e349a2bc8059b015191
Blake Hollingsworth <blake.hollingsworth@gmail.com>
2015-04-10 18:01:04 -0400

    initial commit

---
+++ src/DryIocSampleWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DryIocSampleWebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductsController(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IHttpActionResult Get()
+        {
+            return Ok(_productRepository.List());
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var found = _productRepository.GetById(id);
+
+            if (found == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(found);
+        }
+    }
+}
---
+++ src/DryIocSampleWebApi/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DryIocSampleWebApi
+{
+    public class ProductRepository
+    {
+        private readonly IList<Product> _products = new List<Product>
+        {
+            new Product {Id = 1, Name = "Bubblegum"},
+            new Product {Id = 2, Name = "Candy"}
+        };
+
+        public IEnumerable<Product> List()
+        {
+            return _products;
+        }
+
+        public Product GetById(int id)
+        {
+            return _products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
